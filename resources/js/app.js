2e76d5f81f57781de2512c4bf22fe48d239fe820
iamatnuria <ignasiamat10@gmail.com>
2022-03-25 14:21:41 +0100

    Grid creator done with vanilla JS, AlpineJS integration pending

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,35 @@
-require('./bootstrap');
+let grid = document.querySelector('#game');
+
+// the number of guesses (3)
+let guessesAllowed = 4;
+let wordLength = 3;
+// the length of the word (3)
+
+let fragment = document.createDocumentFragment();
+
+let generateGrid = () => {
+
+Array.from({ length:guessesAllowed }).forEach(() => {
+    let row = document.createElement('div');
+    row.classList.add('row');
+
+    Array.from({ length: wordLength }).forEach(() => {
+        let tile = document.createElement('div');
+        tile.classList.add('tile');
+
+        row.appendChild(tile);
+    });
+
+    fragment.appendChild(row);
+});
+
+grid.append(fragment);
+
+}
+
+generateGrid();
+
+// where each row contains 3 columns
+// append to the grid
+
+
